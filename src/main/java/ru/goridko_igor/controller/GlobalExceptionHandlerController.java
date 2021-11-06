3a0a984d98b56ce7738bdc44c204ddd7d2876123
Goridko-Igor <Goridko-Igor-1917@yandex.ru>
2021-11-06 05:49:55 +0400

    TASK-1_repository_initialization Удалены ненужные описания контроллера GlobalExceptionHandlerController для Swagger.

--- src/main/java/ru/goridko_igor/controller/GlobalExceptionHandlerController.java
+++ src/main/java/ru/goridko_igor/controller/GlobalExceptionHandlerController.java
@@ -1,7 +1,5 @@
 package ru.goridko_igor.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.goridko_igor.exception.PriorityNotFoundException;
@@ -14,58 +12,48 @@ import ru.goridko_igor.exception.TaskNotFoundException;
 import ru.goridko_igor.exception.TaskTypeNotFoundException;
 import ru.goridko_igor.exception.UserNotFoundException;
 
-@Tag(name = "Обработчики ошибок")
 @ControllerAdvice
 public class GlobalExceptionHandlerController {
-    @Operation(summary = "Обработать PriorityNotFoundException")
     @ExceptionHandler(PriorityNotFoundException.class)
     public void handlePriorityNotFoundException(PriorityNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать ProjectNotFoundException")
     @ExceptionHandler(ProjectNotFoundException.class)
     public void handleProjectNotFoundException(ProjectNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать ProjectParticipantNotFoundException")
     @ExceptionHandler(ProjectParticipantNotFoundException.class)
     public void handleProjectParticipantNotFoundException(ProjectParticipantNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать ReleaseVersionNotFoundException")
     @ExceptionHandler(ReleaseVersionNotFoundException.class)
     public void handleReleaseVersionNotFoundException(ReleaseVersionNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать RoleNotFoundException")
     @ExceptionHandler(RoleNotFoundException.class)
     public void handleRoleNotFoundException(RoleNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать StatusNotFoundException")
     @ExceptionHandler(StatusNotFoundException.class)
     public void handleStatusNotFoundException(StatusNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать TaskNotFoundException")
     @ExceptionHandler(TaskNotFoundException.class)
     public void handleTaskNotFoundException(TaskNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать TaskTypeNotFoundException")
     @ExceptionHandler(TaskTypeNotFoundException.class)
     public void handleTaskTypeNotFoundException(TaskTypeNotFoundException e) {
 
     }
 
-    @Operation(summary = "Обработать UserNotFoundException")
     @ExceptionHandler(UserNotFoundException.class)
     public void handleUserNotFoundException(UserNotFoundException e) {
 
